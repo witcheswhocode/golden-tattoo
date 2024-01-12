4a8d1d184f50e918507f930458dcdab1237237ae
Liz Anderson <lizanderson.ca@gmail.com>
2024-01-12 13:25:19 -0800

    Selection state

--- src/components/BraceletIdeas.tsx
+++ src/components/BraceletIdeas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface Bracelet {
   name: string;
@@ -9,19 +9,31 @@ interface BraceletIdeasProps {
   letters: any;
 }
 
-const BraceletIdeas: React.FC<BraceletIdeasProps> = ({ bracelets, letters }) => {
-  const [braceletQuantities, setBraceletQuantities] = useState<{ [key: string]: number }>(
+const BraceletIdeas: React.FC<BraceletIdeasProps> = ({
+  bracelets,
+  letters,
+}) => {
+  const [braceletQuantities, setBraceletQuantities] = useState<{
+    [key: string]: number;
+  }>(
     bracelets.reduce((acc: any, bracelet) => {
       acc[bracelet] = 0;
       return acc;
     }, {})
   );
+  const [braceletSelection, setBraceletSelection] = useState<{
+    [key: string]: number;
+  }>();
 
   const handleIncrement = (id: string) => {
-    console.log(id)
     setBraceletQuantities((prevQuantities) => ({
       ...prevQuantities,
       [id]: prevQuantities[id] + 1,
+    }));
+
+    setBraceletSelection((prevSelection) => ({
+      ...(prevSelection || {}), // Use an empty object as a default if prevQuantities is null
+      [id]: prevSelection && prevSelection[id] ? prevSelection[id] + 1 : 1,
     }));
   };
 
@@ -30,10 +42,39 @@ const BraceletIdeas: React.FC<BraceletIdeasProps> = ({ bracelets, letters }) => 
       ...prevQuantities,
       [id]: prevQuantities[id] > 0 ? prevQuantities[id] - 1 : 0,
     }));
+
+    setBraceletSelection((prevSelection) => ({
+      ...(prevSelection || {}), // Use an empty object as a default if prevQuantities is null
+      [id]: prevSelection && prevSelection[id] ? prevSelection[id] - 1 : 0,
+    }));
+
+    console.log(braceletSelection)
   };
 
   return (
     <div className="p-4">
+      {braceletSelection && Object.entries(braceletSelection).map(([id, quantity]) => (
+        <div key={`${id}-${quantity}`} className="flex items-center justify-between mb-4">
+          <span className="text-lg">{id}</span>
+          <div className="flex items-center">
+            <button
+              onClick={() => handleDecrement(id)}
+              className="bg-red-500 text-white p-2 rounded-l"
+            >
+              -
+            </button>
+            <span className="px-4">{braceletSelection[id]}</span>
+            <button
+              onClick={() => handleIncrement(id)}
+              className="bg-green-500 text-white p-2 rounded-r"
+            >
+              +
+            </button>
+          </div>
+        </div>
+      ))}
+      <div className="bg-black h-0.5 m-5"></div>
+      {!braceletSelection && <>Bracelet selections will go here.</>}
       {bracelets.map((bracelet) => (
         <div key={bracelet} className="flex items-center justify-between mb-4">
           <span className="text-lg">{bracelet}</span>
