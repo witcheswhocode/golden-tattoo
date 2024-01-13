eed122e03c5a39193f5ade79c2808f635f8abfc8
Liz Anderson <lizanderson.ca@gmail.com>
2024-01-12 19:00:47 -0800

    Update BraceletIdeas.tsx

--- src/components/BraceletIdeas.tsx
+++ src/components/BraceletIdeas.tsx
@@ -167,7 +167,7 @@ const BraceletIdeas: React.FC<BraceletIdeasProps> = ({
             <button
               onClick={() => handleDecrement(bracelet)}
               className={`bg-red-500 text-white p-2 rounded-l ${
-                braceletQuantities[bracelet].active && braceletQuantities[bracelet].value !== 0 ? "" : "opacity-25 cursor-not-allowed"
+                braceletQuantities[bracelet].active && braceletQuantities[bracelet].value !== 0 ? "" : "cursor-not-allowed"
               }`}
             >
               -
