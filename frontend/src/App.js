a8a8402d046d78bed9e8f3d8a68e0cc70aca5b83
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-20 18:51:47 -0700

    resolve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Writers from "./pages/Writers";
 import Bracelets from "./pages/Bracelets";
 import LyricsTable from "./pages/Lyrics";
 import Navbar from "./components/Navbar";
-import Dropdown from "./components/Dropdown/Dropdown";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   const dropdownItems = [
