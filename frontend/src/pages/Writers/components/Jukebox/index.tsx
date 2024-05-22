092315de775fe83ad396e1d42edf5851f9cf1c6f
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-22 16:34:23 -0700

    album imgs in album header

--- frontend/src/pages/Writers/components/Jukebox/index.tsx
+++ frontend/src/pages/Writers/components/Jukebox/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import internal from "stream";
 
 export interface WritersProps {
   alb: string;
@@ -38,28 +39,40 @@ function Jukebox(props: WritersData) {
     "Joe Alwyn",
   ];
 
-  function getColor(writer: string){
-    if (listOfPromeninetWriters.includes(writer)){
+  function getColor(writer: string) {
+    if (listOfPromeninetWriters.includes(writer)) {
       return writer.replaceAll(" ", "");
-    }
-    else return 'Default';
+    } else return "Default";
   }
+
   const createDiv = (
     albumshort: string,
     released: number | null,
-    count: string | null
+    count: string | null,
+    alb: string | null
   ) => {
     const keySuffix =
       (released !== null ? `-${released}` : "") +
       (count !== null ? `-${count}` : "");
 
     return (
-      <div key={`${albumshort}${keySuffix}`}>
-        <div className="text-center text-xl">{`${albumshort}`}</div>
+      <section aria-label={`${albumshort}`} key={`${albumshort}${keySuffix}`}>
+        <div className="mb-10 px-4 pt-8 border-2 border-solid border-black border-b-10">
+          <img
+            className="m-1"
+            loading="lazy"
+            src={`assets/albums/${alb}.png`}
+            alt="Album Image"
+          />
+          <div className="text-center text-xl">{`${albumshort}`}</div>
+        </div>
         {props.data
           .filter((item) => item.albumshort === albumshort)
           .map((filteredItem) => (
-            <div className="mb-1 flex bg-white rounded-full transition-height duration-600 ease-in pb-1 overflow-hidden" key={`${filteredItem.song}${keySuffix}`}>
+            <div
+              className="mb-1 flex bg-white rounded-full transition-height duration-600 ease-in pb-1 overflow-hidden"
+              key={`${filteredItem.song}${keySuffix}`}
+            >
               <div className="flex items-center justify-center h-auto w-1/3 min-w-1/3 px-4">
                 <div className="text-center">{filteredItem.song}</div>
               </div>
@@ -69,7 +82,11 @@ function Jukebox(props: WritersData) {
                   .reverse()
                   .map((writer, index) => (
                     <div className="w-auto h-8 hover:h-48 focus:h-48 transition-height duration-300 ease-in-out">
-                      <div className={`bg-${getColor(writer)} min-w-max inline-block mr-2 px-2 py-1 text-center text-black bg-transparent border border-transparent rounded-full text-xs`}>
+                      <div
+                        className={`bg-${getColor(
+                          writer
+                        )} min-w-max inline-block mr-2 px-2 py-1 text-center text-black bg-transparent border border-transparent rounded-full text-xs`}
+                      >
                         {writer}
                       </div>
                     </div>
@@ -77,13 +94,18 @@ function Jukebox(props: WritersData) {
               </div>
             </div>
           ))}
-      </div>
+      </section>
     );
   };
 
   // Get unique album and song combinations
   const uniqueAlbums = [
-    ...new Set(props.data.map((item) => `${item.albumshort}_${item.released}`)),
+    ...new Set(
+      props.data.map(
+        (item) =>
+          `${item.albumshort}_${item.released}_${item.totalwriters}_${item.alb}`
+      )
+    ),
   ];
 
   // Sorting function based on release date
@@ -108,10 +130,11 @@ function Jukebox(props: WritersData) {
   const sortedUniqueAlbums = uniqueAlbums.sort(sortFunction);
   // Render div elements for each unique album and song combination
   const result = sortedUniqueAlbums.map((combination) => {
-    const [albumshort, released, count] = combination.split("_");
+    const [albumshort, released, count, alb] = combination.split("_");
+    console.log(combination);
     const releasedNumber = released === "NaN" ? parseInt(released, 10) : 5000;
 
-    return createDiv(albumshort, releasedNumber, count);
+    return createDiv(albumshort, releasedNumber, count, alb);
   });
 
   // Toggle sorting order when the button is clicked
