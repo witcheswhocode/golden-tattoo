8bfd61ad4ea5ee6b4b721ba8d0dba257117be0f4
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-28 18:30:11 -0700

    break out song item

---
+++ frontend/src/pages/Writers/components/Jukebox/components/SongItem/index.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { writerDetails } from "../../constants";
+
+export interface SongItemProps {
+  song: string;
+  writers: string;
+}
+
+function SongItem(props: SongItemProps) {
+  const listOfPromeninetWriters = [
+    "Taylor Swift",
+    "Liz Rose",
+    "Jack Antonoff",
+    "Max Martin",
+    "Shellback",
+    "Aaron Dessner",
+    "Joe Alwyn",
+  ];
+
+  function getColor(writer: string) {
+    if (listOfPromeninetWriters.includes(writer)) {
+      return writer.replaceAll(" ", "");
+    } else return "Default";
+  }
+  return (
+    <div
+      className="mb-1 flex bg-white rounded-lg hover:rounded-none transition-all duration-600 ease-in pb-1 overflow-hidden"
+      key={`${props.song}`}
+    >
+      <div className="w-32 overflow-hidden whitespace-nowrap hover:w-64 focus:w-64 transition-width duration-300 ease-in-out">
+        <div className="text-center">{props.song}</div>
+      </div>
+      <div className="writers flex w-full px-2 overflow-x-auto overflow-y-hidden">
+        {props.writers
+          .split(",")
+          .reverse()
+          .map((writer, index) => {
+            const writerDetail = writerDetails[writer.trim()]; // Use trim to handle any extra spaces around the writer names
+            const imgUrl =
+              writerDetail?.imgurl ||
+              "https://i.pinimg.com/736x/2a/77/a6/2a77a6fb864f2b974952c30270ccf001.jpg"; // Use optional chaining to handle cases where writerDetail might be undefined
+            return (
+              <div
+                key={index}
+                className="w-auto h-8 flex flex-col items-center hover:h-48 focus:h-48 transition-height duration-300 ease-in-out"
+              >
+                <div
+                  className={`bg-${getColor(
+                    writer
+                  )} min-w-max inline-block mr-2 mb-4 px-2 py-1 text-center text-black bg-transparent border border-transparent rounded-full text-base`}
+                >
+                  {writer}
+                </div>
+                <div className="w-24 h-24 bg-gray-200">
+                  {imgUrl ? (
+                    <img
+                      className="w-24 h-24 object-cover"
+                      loading="lazy"
+                      src={imgUrl}
+                      alt={writer}
+                    />
+                  ) : (
+                    <span className="text-gray-500">No Image Available</span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default SongItem;
--- frontend/src/pages/Writers/components/Jukebox/index.tsx
+++ frontend/src/pages/Writers/components/Jukebox/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AlbumHeader from "../AlbumHeader";
-import { writerDetails } from "./constants";
+import SongItem from "./components/SongItem";
 
 export interface WritersProps {
   alb: string | null;
@@ -30,21 +30,7 @@ export interface WritersData {
 
 function Jukebox(props: WritersData) {
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc"); // Track sorting order
-  const listOfPromeninetWriters = [
-    "Taylor Swift",
-    "Liz Rose",
-    "Jack Antonoff",
-    "Max Martin",
-    "Shellback",
-    "Aaron Dessner",
-    "Joe Alwyn",
-  ];
-
-  function getColor(writer: string) {
-    if (listOfPromeninetWriters.includes(writer)) {
-      return writer.replaceAll(" ", "");
-    } else return "Default";
-  }
+  
 
   const createDiv = (
     albumshort: string,
@@ -78,53 +64,7 @@ function Jukebox(props: WritersData) {
         {props.data
           .filter((item) => item.albumshort === albumshort)
           .map((filteredItem) => (
-            <div
-              className="mb-1 flex bg-white rounded-lg hover:rounded-none transition-all duration-600 ease-in pb-1 overflow-hidden"
-              key={`${filteredItem.song}${keySuffix}`}
-            >
-              <div className="w-32 overflow-hidden whitespace-nowrap hover:w-64 focus:w-64 transition-width duration-300 ease-in-out">
-                <div className="text-center">{filteredItem.song}</div>
-              </div>
-              <div className="writers flex w-full px-2 overflow-x-auto overflow-y-hidden">
-                {filteredItem.writers
-                  .split(",")
-                  .reverse()
-                  .map((writer, index) => {
-                    const writerDetail = writerDetails[writer.trim()]; // Use trim to handle any extra spaces around the writer names
-                    const imgUrl =
-                      writerDetail?.imgurl ||
-                      "https://i.pinimg.com/736x/2a/77/a6/2a77a6fb864f2b974952c30270ccf001.jpg"; // Use optional chaining to handle cases where writerDetail might be undefined
-                    return (
-                      <div
-                        key={index}
-                        className="w-auto h-8 flex flex-col items-center hover:h-48 focus:h-48 transition-height duration-300 ease-in-out"
-                      >
-                        <div
-                          className={`bg-${getColor(
-                            writer
-                          )} min-w-max inline-block mr-2 mb-4 px-2 py-1 text-center text-black bg-transparent border border-transparent rounded-full text-base`}
-                        >
-                          {writer}
-                        </div>
-                        <div className="w-24 h-24 bg-gray-200">
-                          {imgUrl ? (
-                            <img
-                              className="w-24 h-24 object-cover"
-                              loading="lazy"
-                              src={imgUrl}
-                              alt={writer}
-                            />
-                          ) : (
-                            <span className="text-gray-500">
-                              No Image Available
-                            </span>
-                          )}
-                        </div>
-                      </div>
-                    );
-                  })}
-              </div>
-            </div>
+            <SongItem song={filteredItem.song} writers={filteredItem.writers} />
           ))}
       </section>
     );
