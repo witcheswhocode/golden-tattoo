05e272a2880ec00373a62db31d715bff5ffcd3ac
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-30 14:06:19 -0700

    edit some names and styles

--- frontend/src/pages/Bracelets/components/BraceletIdeas/index.tsx
+++ frontend/src/pages/Bracelets/components/BraceletIdeas/index.tsx
@@ -188,22 +188,24 @@ const BraceletIdeas: React.FC<BraceletIdeasProps> = ({
       </ExpandableDiv>
 
       <Tabs>
-        <Tab label="label">
-          <ListOfBestCombinations
-            combinations={mostLettersUsed}
-            title={"Most Letters"}
-          />
-
-          <ListOfBestCombinations
-            combinations={mostBraceletOptions}
-            title={"Most Bracelets"}
-          />
-        </Tab>
-        <Tab label="label12">
+        <Tab label="Build Your Own">
           <ListOfBraceletIdeas
             braceletQuantities={braceletQuantities}
             handleDecrement={handleDecrement}
             handleIncrement={handleIncrement}
+          />
+        </Tab>
+        <Tab label="Optimized Combos">
+          <ListOfBestCombinations
+            combinations={mostLettersUsed}
+            title={"Use the Most Letters"}
+            desc={"This list gives you the variation that will use the most beads."}
+          />
+
+          <ListOfBestCombinations
+            combinations={mostBraceletOptions}
+            title={"Make the Most Bracelets"}
+            desc={"This is a list a various combinations you can try to make the most bracelets."}
           />
         </Tab>
       </Tabs>
--- frontend/src/pages/Bracelets/components/ListOfBestCombinations/index.tsx
+++ frontend/src/pages/Bracelets/components/ListOfBestCombinations/index.tsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from "react";
 interface ListOfBestCombinationsProps {
   combinations: string[][] | null;
   title: string;
+  desc: string;
 }
 
 const ListOfBestCombinations: React.FC<ListOfBestCombinationsProps> = ({
   combinations,
   title,
+  desc
 }) => {
   return (
     <div className="p-4">
-      <div className="font-bold">{title}</div>
+      <div className="font-bold my-1">{title}</div>
+      <div className="text-xs my-1 mb-2">{desc}</div>
       <div>
         {combinations?.map((item, index) => (
-          <div className="border" key={index}>
+          <div className="my-4 border-bottom" key={index}>
             {item.map((word, wordIndex) => (
               <div key={wordIndex}>{word}</div>
             ))}
