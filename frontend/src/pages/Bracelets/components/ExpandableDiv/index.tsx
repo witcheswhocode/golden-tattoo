888548e1d86a24d2f33feb5f4c78172c9b0eb6a7
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-30 14:27:12 -0700

    Update index.tsx

--- frontend/src/pages/Bracelets/components/ExpandableDiv/index.tsx
+++ frontend/src/pages/Bracelets/components/ExpandableDiv/index.tsx
@@ -17,17 +17,31 @@ const ExpandableDiv: React.FC<ExpandableDivProps> = ({ children, count }) => {
   return (
     <div className="z-10 fixed bottom-0 left-0 w-full p-4 bg-white shadow-lg">
       <div
-        className={`flex justify-between items-center cursor-pointer bg-white ${
-          isExpanded ? "border-b-2 border-gray-600" : ""
-        }`}
+        className={`flex justify-between items-center cursor-pointer bg-white`}
         onClick={toggleExpansion}
       >
         <h2 className="text-lg font-semibold bg-white">Selected Bracelets</h2>
         <span>{count !== undefined ? `${count} selected` : `0 selected`}</span>
+        <div className={`transform transition-transform duration-300 ${isExpanded ? 'rotate-180' : 'rotate-0'}`}>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
       </div>
       <div
         className={`overflow-hidden transition-all duration-300 ${
-          isExpanded ? "max-h-screen" : "max-h-0"
+          isExpanded ? "max-h-screen border-t-2 border-gray-600 mt-4" : "max-h-0"
         } bg-white`}
       >
         {count ? (
