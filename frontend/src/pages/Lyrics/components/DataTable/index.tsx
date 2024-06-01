c5610da745a4f6c5830046d001cbcd0c8acfa378
Liz Anderson <lizanderson.ca@gmail.com>
2024-05-31 17:44:50 -0700

    Apply lyrics page design (#4)
    * Update index.tsx
    
    * add rest of categories and style them
    
    * add styles and fix category pills
    
    * add emojis to table
    
    * category filter working a little better
    
    * filter by category works as intended
    
    * desktop view of categories
    
    * Update index.tsx
    
    * add album colors, fix size of desktop table width
    
    * add colors to song titles in modal
    
    * more work on modal shadows
    
    * add word to modal
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * add intro text
    
    * Update index.tsx
    
    * underline active navlink

--- frontend/src/pages/Lyrics/components/DataTable/index.tsx
+++ frontend/src/pages/Lyrics/components/DataTable/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Pagination from "../Pagination";
 import Table from "../Table";
+import { useTheme } from "src/components/ThemeContext";
 
 export interface TableRow {
   wordid: number;
@@ -47,23 +48,56 @@ const getConsecutiveMatches = (str: string, searchTerm: string) => {
   return maxCount;
 };
 
-const allCategories = ["parallels", "transportation", "queer"];
+const allCategories = [
+  "all categories",
+  "parallels",
+  "literature",
+  "color",
+  "queer",
+  "date",
+  "number",
+  "explicit",
+  "alcohol",
+  "hiding",
+  "secrets",
+  "family",
+  "body",
+  "reflective light",
+  "place",
+  "transportation",
+  "person",
+  "games",
+  "spiritual",
+  "elements",
+  "space",
+  "kingdom",
+  "no category",
+];
 
 const DataTable: React.FC<DataTableProps> = (props: DataTableProps) => {
+  const { theme } = useTheme();
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(20);
   const [searchTerm, setSearchTerm] = useState("");
   const [sortColumn, setSortColumn] = useState<string | null>(null);
-  const [sortCategory, setSortCategory] = useState<string | null>(null);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+  const [selectedCategories, setSelectedCategories] =
+    useState<string[]>(allCategories);
+  const numberOfCategories = allCategories.length;
+  const [clickedCategory, setClickedCategory] = useState<Boolean>(false);
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const hasCommonElement = (arr1: string[], arr2: string[]): boolean => {
+    return arr1.some((element) => arr2.includes(element));
+  };
+
   const filteredData = props.data
     .filter(
       (item) =>
         item.otherwords.toLowerCase().includes(searchTerm.toLowerCase()) &&
-        (sortCategory === null || (item.categories && item.categories.split('|').map(cat => cat.trim()).includes(sortCategory)))
+        item.categories &&
+        hasCommonElement(item.categories.split("|"), selectedCategories)
     )
     .sort((a, b) => {
       const startsWithSearchTermA = a.otherwords
@@ -133,31 +167,57 @@ const DataTable: React.FC<DataTableProps> = (props: DataTableProps) => {
   };
 
   const handleCategoryClick = (category: string) => {
-    if (category === sortCategory) {
-      setSortCategory(null);
+    if (!clickedCategory) {
+      setClickedCategory(true);
+      setSelectedCategories([category]);
+    }
+    if (category === "all categories") {
+      setClickedCategory(false);
+      setSelectedCategories(allCategories);
     } else {
-      setSortCategory(category);
+      if (selectedCategories.includes(category)) {
+        // Deselect the category if it's already selected
+        setSelectedCategories(selectedCategories.filter((c) => c === category));
+      } else {
+        // Select the category if it's not already selected
+        setSelectedCategories([...selectedCategories, category]);
+      }
     }
   };
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+      <div className="w-full flex justify-center items-center">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className={`border-2 p-1 m-2 mb-4 md:w-1/3`}
+        />
+      </div>
+      <div className="flex flex-row flex-nowrap w-full mb-4 overflow-auto gap-1 md:flex-wrap md:justify-center">
+        {allCategories.map((category, index) => (
+          <div
+            className={`flex flex-nowrap justify-center items-center w-auto px-3 py-1 text-sm text-white rounded-full cursor-pointer whitespace-nowrap category-color-${
+              ["queer", "midnights"].includes(category)
+                ? category
+                : index > 7
+                ? `100`
+                : index
+            }  ${selectedCategories.includes(category) ? "selected" : ""}`}
+            key={category}
+            onClick={() => handleCategoryClick(category)}
+          >
+            {category}
+          </div>
+        ))}
+      </div>
+      <Table
+        openModal={handleOpenModal}
+        headerClick={handleHeaderClick}
+        data={currentItems}
       />
-      {allCategories.map((category) => (
-        <div
-          className="cursor-pointer"
-          key={category}
-          onClick={() => handleCategoryClick(category)}
-        >
-          {category}
-        </div>
-      ))}
-      <Table openModal={handleOpenModal} headerClick={handleHeaderClick} data={currentItems} />
       <Pagination
         pages={Array.from(Array(props.data.length).keys()).map((x) => x++)}
         totalItems={props.data.length}
