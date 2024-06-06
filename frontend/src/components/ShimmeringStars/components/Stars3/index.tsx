7713908c1aafba4b22968677566a917052d8dfee
Liz Anderson <lizanderson.ca@gmail.com>
2024-06-05 23:03:27 -0700

    randomly generate dots for midnights background

--- frontend/src/components/ShimmeringStars/components/Stars3/index.tsx
+++ frontend/src/components/ShimmeringStars/components/Stars3/index.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import { useTheme } from 'src/components/ThemeContext';
+import React, { useState, useEffect } from "react";
+import { useTheme } from "src/components/ThemeContext";
+
+const getRandomValue = (min: number, max: number) => Math.random() * (max - min) + min;
+const getRandomDelay = (min: number, max: number) => Math.random() * (max - min) + min;
+
+const generateStarPositions = (numStars: number, viewportWidth: number, viewportHeight: number) => {
+  const starPositions = [];
+  for (let i = 0; i < numStars; i++) {
+    const top = getRandomValue(0, viewportHeight);
+    const left = getRandomValue(0, viewportWidth);
+    const delay = getRandomDelay(0, 2000); // Delay between 0 and 2000ms
+    starPositions.push({ top, left, delay });
+  }
+  return starPositions;
+};
 
 const Stars3: React.FC = () => {
   const { theme } = useTheme();
-  const starPositions: { top: number; left: number, delay: number }[] = [
-    { top: 88, left: 162, delay: 500 },
-    { top: 128, left: 42, delay: 900 },
-    { top: 21, left: 162, delay: 1600 }
-    // Add more star positions as needed
-  ];
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  const [starPositions, setStarPositions] = useState<{ top: number; left: number; delay: number }[]>([]);
+
+  useEffect(() => {
+    const updateStarPositions = () => {
+      const positions = generateStarPositions(50, viewportWidth, viewportHeight); // Generate 50 stars
+      setStarPositions(positions);
+    };
+
+    updateStarPositions();
+
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+      setViewportHeight(window.innerHeight);
+      updateStarPositions();
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [viewportWidth, viewportHeight]);
 
   return (
-    <div className="fixed w-full h-full">
+    <div className="fixed w-full h-full z-0">
       {starPositions.map((position, index) => (
         <div
           key={index}
-
-          className={`absolute w-1 h-1 bg-white rounded-full animate-glowing-stars ${theme === 'midnights' ? '' : 'hidden'}`}
+          className={`absolute w-1 h-1 bg-white rounded-full animate-glowing-stars ${
+            theme === "midnights" ? "" : "hidden"
+          }`}
           style={{
             top: `${position.top}px`,
             left: `${position.left}px`,
-            animationDelay: `${position.delay}ms`
+            animationDelay: `${position.delay}ms`,
           }}
         />
       ))}
