2d59dd7d19cb306eaf51922e619bd60461e5af7b
Liz Anderson <lizanderson.ca@gmail.com>
2024-06-15 18:29:07 -0700

    set up analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Navigate,
   Route,
   Routes,
+  useLocation,
 } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Writers from "./pages/Writers";
@@ -18,12 +20,35 @@ import ShimmeringStars from "./components/ShimmeringStars";
 import About from "./pages/About";
 import Typewriter from "./components/Typewriter";
 import RandomTypewriterBackground from "./components/RandomTypewriterBackground";
+import initializeAnalytics from "./analytics";
+
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+};
 
 function App() {
+  useEffect(() => {
+    initializeAnalytics();
+    // Track the initial page load
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+    });
+  }, []);
+
   return (
     <ThemeProvider>
       <ThemeLoader />
-      <ThemeWrapper />
+      <Router>
+        <ThemeWrapper />
+      </Router>
     </ThemeProvider>
   );
 }
@@ -31,6 +56,8 @@ function App() {
 function ThemeWrapper() {
   const { theme } = useTheme();
   const [dropDownClicked, setDropDownClicked] = useState<boolean>(false);
+
+  usePageTracking();
 
   const ThemeToggle = () => {
     const { theme, toggleTheme } = useTheme();
@@ -74,20 +101,19 @@ function ThemeWrapper() {
           : `bg-${theme}-background`
       }  flex flex-col justify-center items-center min-h-screen`}
     >
-      <Router>
-        <Header />
-        <Navbar />
-        <ThemeToggle />
-        <AppContent />
-        <Footer />
-        <ShimmeringStars />
-      </Router>
+      <Header />
+      <Navbar />
+      <ThemeToggle />
+      <AppContent />
+      <Footer />
+      <ShimmeringStars />
     </div>
   );
 }
 
 function AppContent() {
   const { theme } = useTheme();
+
   return (
     <Routes>
       <Route path="/writers" element={<Writers />} />
---
+++ frontend/src/analytics.js
@@ -0,0 +1,7 @@
+import ReactGA from "react-ga4";
+
+const initializeAnalytics = () => {
+  ReactGA.initialize("G-ZD7FLH0WCL");
+};
+
+export default initializeAnalytics;
