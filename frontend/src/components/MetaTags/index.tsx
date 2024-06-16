d85f64352df51f68dad1e448e96e00a18e526420
Liz Anderson <lizanderson.ca@gmail.com>
2024-06-16 14:22:16 -0700

    hopeful metatag fix

--- frontend/src/components/MetaTags/index.tsx
+++ frontend/src/components/MetaTags/index.tsx
@@ -1,3 +1,4 @@
+// MetaTags.tsx
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -8,7 +9,10 @@ interface MetaTagsProps {
 }
 
 const MetaTags: React.FC<MetaTagsProps> = ({ title, description, image }) => {
-    console.log(`${process.env.PUBLIC_URL}${image}`)
+    // Construct absolute URL for the image
+    const imageUrl = `${window.location.origin}${process.env.PUBLIC_URL}${image}`;
+    console.log(imageUrl)
+
     return (
         <Helmet>
             <title>{title}</title>
@@ -19,7 +23,7 @@ const MetaTags: React.FC<MetaTagsProps> = ({ title, description, image }) => {
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={`${process.env.PUBLIC_URL}${image}`} />
+            <meta property="og:image" content={imageUrl} />
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
 
@@ -27,7 +31,7 @@ const MetaTags: React.FC<MetaTagsProps> = ({ title, description, image }) => {
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:title" content={title} />
             <meta property="twitter:description" content={description} />
-            <meta property="twitter:image" content={`${process.env.PUBLIC_URL}${image}`} />
+            <meta property="twitter:image" content={imageUrl} />
         </Helmet>
     );
 };
